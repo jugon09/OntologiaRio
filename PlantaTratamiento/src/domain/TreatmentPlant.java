6db0a253c4fae9c06a8839198d0918c753dd6dc6
Julian <julian4suarez@gmail.com>
2016-06-17 21:22:24 +0200

    Efficiency of TreatmentPlant

--- PlantaTratamiento/src/domain/TreatmentPlant.java
+++ PlantaTratamiento/src/domain/TreatmentPlant.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class TreatmentPlant {
 	private ArrayList<Treatment> tratamientos;
 	private ArrayList<Tank> tanques;
-	
+
 	public TreatmentPlant(ArrayList<Treatment> t, ArrayList<Tank> tan) {
 		tratamientos = t;
 		tanques = tan;
@@ -18,7 +18,7 @@ public class TreatmentPlant {
 	public void setTratamientos(ArrayList<Treatment> tratamientos) {
 		this.tratamientos = tratamientos;
 	}
-	
+
 	public ArrayList<Tank> getTanques() {
 		return tanques;
 	}
@@ -27,19 +27,28 @@ public class TreatmentPlant {
 		this.tanques = tanques;
 	}
 
+	public double getTotalapacity() {
+		double capacity = 0;
+		for (Tank tank : tanques) {
+			capacity += tank.getCapacity();
+		}
+		return capacity;
+	}
+
 	public WaterMass applyAllTreatments(WaterMass w) {
 		WaterMass res = new WaterMass(w);
-		for (Treatment t: tratamientos) t.treatWaterMass(res);
+		for (Treatment t : tratamientos)
+			t.treatWaterMass(res);
 		return res;
 	}
-	
-	public WaterMass applyTreatment(WaterMass w,WaterMass obj) {
+
+	public WaterMass applyTreatment(WaterMass w, WaterMass obj) {
 		WaterMass res = new WaterMass(w);
 		int i = 0;
 		int n = tratamientos.size();
 		if (n > 0) {
 			while (res.DBO > obj.DBO && res.DQO > obj.DQO) {
-				tratamientos.get(i%n).treatWaterMass(res);
+				tratamientos.get(i % n).treatWaterMass(res);
 				++i;
 			}
 		}
