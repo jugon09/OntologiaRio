62ee4f04f99d4798433b7966fce39610c93c2bcf
Julian <julian4suarez@gmail.com>
2016-06-18 00:30:32 +0200

    ontology

--- PlantaTratamiento/src/domain/MyTime.java
+++ PlantaTratamiento/src/domain/MyTime.java
@@ -2,9 +2,16 @@ package domain;
 
 public class MyTime {
 	private int tiempo;
-	public MyTime (int t) {
+
+	public MyTime(int t) {
 		tiempo = t;
 	}
-	public int getTime() {return tiempo;}
-	public void addTime(int t) {tiempo += t;}
+
+	public int getTime() {
+		return tiempo;
+	}
+
+	public void addTime(int t) {
+		tiempo += t;
+	}
 }
