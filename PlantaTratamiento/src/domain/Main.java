dd31a3448ce2107f1fcb51f440c66dd6b6f50c5f
jugon09 <camilocastro12@hotmail.com>
2016-06-18 00:21:00 +0200

    funcion tiempo terminada

--- PlantaTratamiento/src/domain/Main.java
+++ PlantaTratamiento/src/domain/Main.java
@@ -1,10 +1,7 @@
 package domain;
 
 import java.lang.reflect.Method;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
